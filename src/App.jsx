17b17d9e9bac2283c7dbbb7629b7227cc1f64741
mohhaych <mohamed54641@gmail.com>
2024-08-06 14:14:17 +0100

    Add Experience component and update Technologies component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Experience from "./components/Experience";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Technologies from "./components/Technologies";
@@ -18,6 +19,7 @@ const App = () => {
         <Hero />
         <About />
         <Technologies />
+        <Experience />
       </div>
     </div>
   )
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,8 +1,7 @@
 import { RiReactjsLine } from 'react-icons/ri';
-import { TbBrandNextJs } from 'react-icons/tb';
-import { SiMongodb } from 'react-icons/si';
+import { TbBrandNextjs } from 'react-icons/tb';   
 import { DiRedis } from 'react-icons/di';
-import { FaNodeJS } from 'react-icons/fa';
+import { FaNodeJs } from 'react-icons/fa';
 import { BiLogoPostgresql } from 'react-icons/bi';
 
 const Technologies = () => {
@@ -14,20 +13,20 @@ const Technologies = () => {
                 <RiReactjsLine className='text-7xl text-cyan-400'/>
             </div>
             <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <RiReactjsLine className='text-7xl text-cyan-400'/>
+                <TbBrandNextjs className='text-7xl'/>
             </div>
             <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <RiReactjsLine className='text-7xl text-cyan-400'/>
+                <DiRedis className='text-7xl text-red-700'/>
             </div>
             <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <RiReactjsLine className='text-7xl text-cyan-400'/>
+                <FaNodeJs className='text-7xl text-green-500'/>
             </div>
             <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <RiReactjsLine className='text-7xl text-cyan-400'/>
+                <BiLogoPostgresql className='text-7xl text-sjy-700'/>
             </div>
         </div>
     </div>
   )
 }
 
-export default Technologies
+export default Technologies 
