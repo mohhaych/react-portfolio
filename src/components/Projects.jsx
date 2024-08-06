d4b925025dfbc9c5c4ff1507068848dddde8c237
mohhaych <mohamed54641@gmail.com>
2024-08-06 14:45:18 +0100

    Refactor Projects component to include project details in Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,9 +12,20 @@ const Projects = () => {
                         src={project.image}
                         width={150}
                         height={150}
-                        alt={project.title}    
+                        alt={project.title}
+                        className="mb-6 rounded"  
                     />
                 </div>
+                    <div className="w-full max-w-xl lg:w-3/4">
+                        <h6 className="mb-2 font-semibold">{project.title}</h6>
+                        <p className="mb-4 text-neutral-400">{project.description}</p>
+                        {project.technologies.map((tech, index) => (
+                            <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900"
+                            >
+                            {tech}
+                            </span>
+                        ))}
+                    </div>
                 </div>
             ))}
             </div>
