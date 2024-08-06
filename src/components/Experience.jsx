6509a2608d2b3eacb2ed0ccc093647a4244290ef
mohhaych <mohamed54641@gmail.com>
2024-08-06 14:26:41 +0100

    Update Experience component to include description and technologies

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -17,6 +17,12 @@ const Experience = () => {
                                 {experience.company}
                             </span>
                         </h6>
+                        <p className="mb-4 text-neutral-400">{experience.description}</p>
+                        {experience.technologies.map((tech, index) => (
+                            <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2
+                            py-1 text-sm font-medium text-purple-800">{tech}
+                            </span>
+                        ))}
                     </div>
                 </div>
             ))}
